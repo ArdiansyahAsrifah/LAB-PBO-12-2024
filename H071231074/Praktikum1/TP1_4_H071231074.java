0c26880b6a6995467aaf61133d63ffa2298ad538
xebec51 <rinaldi.ruslan51@gmail.com>
2024-03-28 00:29:36 +0800

    Upload Praktikum-2 dan Upload Ulang Praktikum-1

---
+++ H071231074/Praktikum1/TP1_4_H071231074.java
@@ -0,0 +1,27 @@
+package Praktikum1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TP1_4_H071231074 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Masukkan jari-jari lingkaran: ");
+            double jariJari = scanner.nextDouble();
+
+            double luas = hitungLuasLingkaran(jariJari);
+            System.out.printf("Luas lingkaran adalah %.2f\n", luas);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Input tidak valid: Harap masukkan angka.");
+        } finally {
+            scanner.close();
+        }
+    }
+
+    public static double hitungLuasLingkaran(double jariJari) {
+        return Math.PI * jariJari * jariJari;
+    }
+}
