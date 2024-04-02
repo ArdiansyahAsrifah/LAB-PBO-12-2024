a318e2956652258c3b68af3dc43d57062e03df41
nabilmuflif <nabilmuflif2505@gmail.com>
2024-04-02 11:32:44 +0800

    pratikum 1&2_H07123036

--- H071231036/Praktikum 1/TP1_1_H071231036/no1.java
+++ H071231036/Praktikum 1/TP1_1_H071231036/no1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BilanganChecker {
+public class no1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int jumlahBilanganBulat = 0;
