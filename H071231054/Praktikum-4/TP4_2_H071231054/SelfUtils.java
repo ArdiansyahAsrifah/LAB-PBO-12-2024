9181d71ce5c967d478f5c2f4b1a4c1f23018dabd
kevinryadi <kvnryadi@gmail.com>
2024-05-02 16:04:59 +0800

    Tugas 4 dan 5

---
+++ H071231054/Praktikum-4/TP4_2_H071231054/SelfUtils.java
@@ -0,0 +1,8 @@
+package TP4_2_H071231054;
+
+public class SelfUtils {
+    public static void displaySelfData(){
+        System.out.println("Nama\t: Kevin Ryadi");
+        System.out.println("NIM\t: H071231054");
+    }
+}
